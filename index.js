ebb4d2e20ffa48a3efc98dee0114c38f53db1420
neerajdask <iamneerajdas@gmail.com>
2021-04-26 13:41:15 +0530

    Firebase initialization and first route

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ const express = require('express');
 const admin = require('firebase-admin');
 const cors = require('cors');
 
+const serviceAccount = require('./config/serviceAccountKey.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://construyo-coding-challenge.firebaseio.com',
+});
+
 const app = express();
 const PORT = 3000 || process.env.port;
 
 app.use(express.json());
+const db = admin.firestore();
 
-app.get('/', (req, res) => {
-    console.log('Hello world');
+app.post('/orders', async (req, res) => {
+    const resp = await db
+        .collection('orders')
+        .doc(req.body.address.city)
+        .set(req.body);
 
-    res.send('Test');
+    console.log(resp);
+    if (!resp) {
+        return res.status(500).send('Something went wrong');
+    }
+    return res.status(200).send(resp);
 });
 
 app.listen(PORT, () => {
